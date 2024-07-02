ce7b8a09566fe0473be3b1d8d7926494bb6dae26
Tyson Do <doraeminemon@gmail.com>
2024-07-02 15:19:49 +0700

    Make the function testable

--- index.ts
+++ index.ts
@@ -1,38 +1,77 @@
-import axios from "axios";
+import axios, { AxiosResponse } from "axios";
 
-async function findServer() {
-  const servers = [
-    {
-      url: "https://does-not-work.perfume.new",
-      priority: 1,
-    },
-    {
-      url: "https://gitlab.com",
-      priority: 4,
-    },
-    {
-      url: "http://app.scnt.me",
-      priority: 3,
-    },
-    {
-      url: "https://offline.scentronix.com",
-      priority: 2,
-    },
-  ];
-  const requests = servers.map(async (server) => {
-    try {
-      const resp = await axios.get(server.url);
-      return [server.url, server.priority, resp];
-    } catch (err) {
-      return null;
-    }
-  });
-  const responses = await Promise.all(requests);
-  const onlySuccesses = responses.filter((resp): resp is any[] =>
-    Array.isArray(resp)
-  );
-  const finalAnswer = onlySuccesses.sort((a, b) => a[1] - b[1])[0];
-  return finalAnswer;
+const servers = [
+  {
+    url: "https://does-not-work.perfume.new",
+    priority: 1,
+  },
+  {
+    url: "https://gitlab.com",
+    priority: 4,
+  },
+  {
+    url: "http://app.scnt.me",
+    priority: 3,
+  },
+  {
+    url: "https://offline.scentronix.com",
+    priority: 2,
+  },
+];
+
+export type Server = {
+  url: string;
+  priority: number;
+};
+
+export type SuccessfulResponse = [string, number, AxiosResponse<any, any>];
+
+class FindServer {
+  servers: Server[];
+  requests: Promise<SuccessfulResponse>[];
+  responses: (SuccessfulResponse | null)[];
+  successfulResponses: SuccessfulResponse[];
+  sortedResponses: SuccessfulResponse[];
+
+  constructor(servers: Server[]) {
+    this.servers = servers;
+  }
+
+  mapRequest() {
+    this.requests = this.servers.map(
+      async (server): Promise<SuccessfulResponse | null> => {
+        try {
+          const resp = await axios.get(server.url, { timeout: 5000 });
+          return [server.url, server.priority, resp];
+        } catch (err) {
+          return null;
+        }
+      }
+    );
+  }
+
+  async sendRequest() {
+    this.responses = await Promise.all(this.requests);
+  }
+
+  filterSuccessfulRequest() {
+    this.successfulResponses = this.responses.filter(
+      (resp): resp is SuccessfulResponse => Array.isArray(resp)
+    );
+  }
+
+  sortRequests() {
+    this.sortedResponses = this.successfulResponses.sort((a, b) => a[1] - b[1]);
+  }
+
+  async run() {
+    this.mapRequest();
+    await this.sendRequest();
+    this.filterSuccessfulRequest();
+    this.sortRequests();
+    return this.sortedResponses[0];
+  }
 }
 
-findServer().then((resp) => console.log(resp));
+const findServer = new FindServer(servers);
+findServer.run().then((resp) => console.log(resp));
